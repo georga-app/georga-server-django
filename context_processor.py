27dc73623b1cc951c3f41d3c117679a02d163cfa
Nils Witt <nils@nils-witt.de>
2022-03-04 20:58:36 +0100

    updated template routing

---
+++ context_processor.py
@@ -0,0 +1,5 @@
+from publicsite import settings
+
+
+def main(request):
+    return {'GITHUB_URL': settings.REPOSITORY_URL}
--- urls.py
+++ urls.py
@@ -1,13 +1,14 @@
-from django.urls import path, re_path, include
-from .views import index, imprint, data_protection, faq, SignUpView, RegistrationDoneView
-from .forms import SignUpForm
+from django.urls import path, include
+from .views import index, imprint, data_protection, faq, SignUpView, CompanyRegistrationView
+from .forms import SignUpForm, CompanySignUpForm
 
 urlpatterns = [
     path('', index, name='index'),
     path('imprint/', imprint, name='imprint'),
     path('faq/', faq, name='faq'),
     path('data_protection/', data_protection, name='data_protection'),
-    re_path('^accounts/register/$', SignUpView.as_view(form_class=SignUpForm), name='django_registration_register'),
+    path('accounts/register/', SignUpView.as_view(form_class=SignUpForm), name='django_registration_register'),
+    path('accounts/company_register/', CompanyRegistrationView.as_view(form_class=CompanySignUpForm), name='django_company_register'),
     path('accounts/', include('django_registration.backends.activation.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
