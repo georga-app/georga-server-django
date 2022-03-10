acbd8fd3a7c46d597e46ee2e1ca01953da033ef0
Nils Witt <nils@nils-witt.de>
2022-03-10 13:39:47 +0100

    added all models to the admin view
    Signed-off-by: Nils Witt <nils@nils-witt.de>

--- admin.py
+++ admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
-from call_for_volunteers.models import Person
+from call_for_volunteers.models import Person, GeneralWorkAvailability, OpeningTime, SinglePersonUptime, HelpOperation, ActionCategory, PublicationCategory, EquipmentSelf, EquipmentProvided, Restriction, QualificationAdministrative, QualificationHealth, QualificationLicense, QualificationLanguage, QualificationTechnical
 
 admin.site.register(Person)
+admin.site.register(GeneralWorkAvailability)
+admin.site.register(OpeningTime)
+admin.site.register(SinglePersonUptime)
+admin.site.register(HelpOperation)
+admin.site.register(ActionCategory)
+admin.site.register(QualificationTechnical)
+admin.site.register(QualificationLanguage)
+admin.site.register(QualificationLicense)
+admin.site.register(QualificationHealth)
+admin.site.register(QualificationAdministrative)
+admin.site.register(Restriction)
+admin.site.register(EquipmentProvided)
+admin.site.register(EquipmentSelf)
+admin.site.register(PublicationCategory)
