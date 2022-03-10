9d5a3626ee24b27106804caa5d5e07a413250a0b
Nils Witt <nils@nils-witt.de>
2022-03-10 18:32:40 +0100

    removed rest_framework
    Signed-off-by: Nils Witt <nils@nils-witt.de>

--- urls.py
+++ urls.py
@@ -1,48 +1,14 @@
-from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+from django.urls import path
 from graphene_django.views import GraphQLView
-from rest_framework import routers
 
-from . import viewsets
-from .forms import SignUpForm, CompanySignUpForm
-from .views import index, imprint, data_protection, faq, SignUpView, CompanyRegistrationView
-
-router = routers.DefaultRouter()
-router.register(r'persons', viewsets.PersonViewSet)
-router.register(r'generalWorkAvailability', viewsets.GeneralWorkAvailabilityViewSet)
-router.register(r'openingTime', viewsets.OpeningTimeViewSet)
-router.register(r'singlePersonUptime', viewsets.SinglePersonUptimeViewSet)
-router.register(r'helpOperation', viewsets.HelpOperationViewSet)
-router.register(r'actionCategory', viewsets.ActionCategoryViewSet)
-router.register(r'qualificationTechnical', viewsets.QualificationTechnicalViewSet)
-router.register(r'qualificationLanguage', viewsets.QualificationLanguageViewSet)
-router.register(r'qualificationLicense', viewsets.QualificationLicenseViewSet)
-router.register(r'qualificationHealth', viewsets.QualificationHealthViewSet)
-router.register(r'qualificationAdministrative', viewsets.QualificationAdministrativeViewSet)
-router.register(r'restriction', viewsets.RestrictionViewSet)
-router.register(r'equipmentProvided', viewsets.EquipmentProvidedViewSet)
-router.register(r'equipmentSelf', viewsets.EquipmentSelfViewSet)
-router.register(r'publicationCategory', viewsets.PublicationCategoryViewSet)
+from .views import index, imprint, data_protection, faq
 
 urlpatterns = [
     path('', index, name='index'),
     path('imprint/', imprint, name='imprint'),
     path('faq/', faq, name='faq'),
     path('data_protection/', data_protection, name='data_protection'),
-    path('accounts/register/', SignUpView.as_view(form_class=SignUpForm), name='django_registration_register'),
-    path('accounts/company_register/', CompanyRegistrationView.as_view(form_class=CompanySignUpForm), name='django_company_register'),
-    path('accounts/', include('django_registration.backends.activation.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
-
-    # Api
-    path('api/', include(router.urls)),
-    path('api-auth/', include("rest_framework.urls", namespace='rest_framework')),
-    # Api docs
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 
     # GraphQL
     path('graphql/', GraphQLView.as_view(graphiql=True)),
-
 ]
--- views.py
+++ views.py
@@ -1,13 +1,10 @@
 import logging
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.contrib.auth import login, authenticate
-#from django.contrib.auth.forms import UserCreationForm
+
 from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
 from django_registration.backends.activation.views import RegistrationView, ActivationView
-from .forms import SignUpForm, CompanySignUpForm
 
+from .forms import SignUpForm, CompanySignUpForm
 
 logger = logging.getLogger('forms')
 
