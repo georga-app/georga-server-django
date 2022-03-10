c5673dfac4425a58e4dac50d3673c6fd031a810a
Nils Witt <nils@nils-witt.de>
2022-03-10 13:19:11 +0100

    GraphQL schema update
    Signed-off-by: Nils Witt <nils@nils-witt.de>

--- schema_graphql.py
+++ schema_graphql.py
@@ -1,7 +1,7 @@
 import graphene
 from graphene_django import DjangoObjectType
 
-from call_for_volunteers.models import Person
+from call_for_volunteers.models import Person, QualificationLanguage
 
 
 class PersonType(DjangoObjectType):
@@ -10,13 +10,21 @@ class PersonType(DjangoObjectType):
         fields = '__all__'
 
 
+class QualificationLanguageType(DjangoObjectType):
+    class Meta:
+        model = QualificationLanguage
+        fields = '__all__'
+
+
 class Query(graphene.ObjectType):
     persons = graphene.List(PersonType)
-    hello = graphene.String(default_value="Hi!")
+    qualification_languages = graphene.List(QualificationLanguageType)
 
     def resolve_persons(root, info):
-        # We can easily optimize query count in the resolve method
         return Person.objects.all()
+
+    def resolve_qualification_languages(root, info):
+        return QualificationLanguage.objects.all()
 
 
 schema = graphene.Schema(query=Query)
