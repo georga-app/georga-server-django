7cc66fe503132a6b3bd306506e2830f7f9f5723e
Nils Witt <nils@nils-witt.de>
2022-03-12 12:54:22 +0100

    url adjustments
    Signed-off-by: Nils Witt <nils@nils-witt.de>

--- urls.py
+++ urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from graphene_django.views import GraphQLView
 
+from call_for_volunteers.schema_graphql import schema
+
 urlpatterns = [
     # GraphQL
-    path('graphql/', GraphQLView.as_view(graphiql=True)),
+    path('graphql', GraphQLView.as_view(graphiql=True, schema=schema)),
 ]
