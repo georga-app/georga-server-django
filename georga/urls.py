0e613b677fec1c7e2741c20ce44ebcd7e65e91ae
Nils Witt <nils@nils-witt.de>
2022-03-13 21:32:37 +0100

    added admin views
    Signed-off-by: Nils Witt <nils@nils-witt.de>

--- georga/urls.py
+++ georga/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from graphene_django.views import GraphQLView
+from django.contrib import admin
 
 from .schema_graphql import schema
 
 urlpatterns = [
     # GraphQL
     path('graphql', GraphQLView.as_view(graphiql=True, schema=schema)),
+
+    # Admin view
+    path('admin/', admin.site.urls),
 ]
