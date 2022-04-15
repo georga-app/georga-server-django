cd2df4343dd6ac633fcc808887e2db5d7801d12a
Thomas Mielke <thomas@mielke.software>
2022-04-15 18:09:45 +0200

    added basic support for ptvsd

--- manage.py
+++ manage.py
@@ -22,4 +22,23 @@ def main():
 
 
 if __name__ == '__main__':
+
+    # from django.conf import settings
+
+    # enable ptvsd debugging (needs open port 51000)
+    if True:  # settings.DEBUG:
+        try:
+            if True:  # os.environ.get('RUN_MAIN') or os.environ.get('WERKZEUG_RUN_MAIN'):
+                import ptvsd
+                ptvsd.enable_attach(address = ('0.0.0.0', 51000))
+                # uncomment these two lines, if you need to debug initialization code:
+                # ptvsd.wait_for_attach()
+                # ptvsd.break_into_debugger()
+                print("Attached remote debugger")
+        except Exception as ex:
+            if hasattr(ex, 'message'):
+                print('ptvsd debugging not possible:' + ex.message)
+            else:
+                print('ptvsd debugging not possible:' + str(ex))
+
     main()
