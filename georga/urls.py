5789bea1501494d98849d14c1e8024888710416d
Alexander Blum <workispleasure@pleasureiswork.net>
2022-04-16 16:00:51 +0200

    reenables csrf for admin interface

--- georga/urls.py
+++ georga/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from graphene_django.views import GraphQLView
 from django.contrib import admin
+from django.views.decorators.csrf import csrf_exempt
 
 from .schema_graphql import schema
 
 urlpatterns = [
     # GraphQL
-    path('graphql', GraphQLView.as_view(graphiql=True, schema=schema)),
+    path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 
     # Admin view
     path('admin/', admin.site.urls),
