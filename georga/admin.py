348114e30af66f31f4e6ad87e9136a0ac8a5391f
Sarah <github@stoffels.it>
2022-06-26 18:30:40 +0200

    Adapt admin.py according to new models

--- georga/admin.py
+++ georga/admin.py
@@ -3,32 +3,44 @@ from django.contrib import admin
 # Register your models here.
 from .models import (
     Person,
-    HelpOperation,
-    ActionCategory,
+    Device,
+    Resource,
+    Organization,
+    Project,
+    Deployment,
+    Task,
+    TaskType,
+    Schedule,
+    Timeslot,
+    Qualification,
+    QualificationType,
+    Restriction,
+    Role,
     EquipmentSelf,
     EquipmentProvided,
-    Restriction,
-    QualificationAdministrative,
-    QualificationHealth,
-    QualificationLicense,
-    QualificationLanguage,
-    QualificationTechnical,
     Location,
-    Poll,
-    PollChoice,
+    LocationType,
+    Notification,
+    NotificationType,
 )
 
 admin.site.register(Person)
-admin.site.register(HelpOperation)
-admin.site.register(ActionCategory)
-admin.site.register(QualificationTechnical)
-admin.site.register(QualificationLanguage)
-admin.site.register(QualificationLicense)
-admin.site.register(QualificationHealth)
-admin.site.register(QualificationAdministrative)
+admin.site.register(Device)
+admin.site.register(Resource)
+admin.site.register(Organization)
+admin.site.register(Project)
+admin.site.register(Deployment)
+admin.site.register(Task)
+admin.site.register(TaskType)
+admin.site.register(Schedule)
+admin.site.register(Timeslot)
+admin.site.register(Qualification)
+admin.site.register(QualificationType)
 admin.site.register(Restriction)
-admin.site.register(EquipmentProvided)
+admin.site.register(Role)
 admin.site.register(EquipmentSelf)
+admin.site.register(EquipmentProvided)
 admin.site.register(Location)
-admin.site.register(Poll)
-admin.site.register(PollChoice)
+admin.site.register(LocationType)
+admin.site.register(Notification)
+admin.site.register(NotificationType)
