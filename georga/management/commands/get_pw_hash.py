fc3e13f37fadc0307d3d011faabb3023c43ef88b
Sarah <github@stoffels.it>
2022-08-12 17:37:54 +0200

    Cleanup file according to flake8

--- georga/management/commands/get_pw_hash.py
+++ georga/management/commands/get_pw_hash.py
@@ -1,4 +1,3 @@
-import os
 import logging
 from django.core.management.base import BaseCommand
 from django.contrib.auth.hashers import make_password
@@ -13,25 +12,9 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
-            '--log',
-            dest='debug',
-            help='set loglevel to debug',
-        )
-        parser.add_argument(
             'passwd',
             help='password to hash',
         )
 
     def handle(self, *args, **options):
-        """
-        method called first after init
-        - **options can contain parameters:
-        - debug - defines loglevel, if none, default is INFO
-        """
-
-        # manage loglevel
-        loglevel = logging.INFO
-        if options['debug']:
-            loglevel = logging.DEBUG
-
         print(make_password(options['passwd']))	
