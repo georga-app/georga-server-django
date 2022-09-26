35ba34da8a041d4a72404428b323ae2698eade33
Alexander Blum <workispleasure@pleasureiswork.net>
2022-09-26 07:24:03 +0200

    adds ace permitted tests

--- georga/tests/schemas/testACE.py
+++ georga/tests/schemas/testACE.py
@@ -1,4 +1,8 @@
-from . import ListQueryTestCase
+from graphql_jwt.exceptions import JSONWebTokenError
+
+from . import ListQueryTestCase, auth
+from ...models import ACE
+
 
 listAcesQuery = """
 query (
@@ -49,3 +53,70 @@ query (
 
 class ListAcesTestCase(ListQueryTestCase):
     operation = listAcesQuery
+
+    def test_login_required(self):
+        """non authenticated access gets a permission error"""
+        # execute operation
+        result = self.client.execute(self.operation)
+        # assert only one permission error
+        self.assertIsNotNone(result.errors)
+        self.assertEqual(len(result.errors), 1)
+        self.assertIsInstance(result.errors[0].original_error, JSONWebTokenError)
+        # prepare query results
+        data = next(iter(result.data.values()))
+        # assert no data
+        self.assertIsNone(data)
+
+    @auth("helper@georga.test")
+    def test_staff_member_required(self):
+        """non staff member gets an empty result"""
+        # execute operation
+        result = self.client.execute(self.operation)
+        # assert no errors
+        self.assertIsNone(result.errors)
+        # prepare query results
+        data = next(iter(result.data.values()))
+        # assert no results
+        self.assertFalse(data['edges'])
+
+    @auth("organization@georga.test")
+    def test_organization_admin_permitted(self):
+        """organization admin gets all and only permitted entries"""
+        # execute operation
+        result = self.client.execute(self.operation)
+        # assert no errors
+        self.assertIsNone(result.errors)
+        # prepare query results
+        data = next(iter(result.data.values()))
+        result_ids = {edge["node"]["id"] for edge in data["edges"]}
+        expected_ids = {entry.gid for entry in ACE.objects.filter(pk__in=[1, 2, 3])}
+        # assert expected results
+        self.assertSetEqual(result_ids, expected_ids)
+
+    @auth("project@georga.test")
+    def test_project_admin_permitted(self):
+        """project admin gets all and only permitted entries"""
+        # execute operation
+        result = self.client.execute(self.operation)
+        # assert no errors
+        self.assertIsNone(result.errors)
+        # prepare query results
+        data = next(iter(result.data.values()))
+        result_ids = {edge["node"]["id"] for edge in data["edges"]}
+        expected_ids = {entry.gid for entry in ACE.objects.filter(pk__in=[2, 3])}
+        # assert expected results
+        self.assertSetEqual(result_ids, expected_ids)
+
+    @auth("operation@georga.test")
+    def test_operation_admin_permitted(self):
+        """operation admin gets all and only permitted entries"""
+        # execute operation
+        result = self.client.execute(self.operation)
+        # assert no errors
+        self.assertIsNone(result.errors)
+        # prepare query results
+        data = next(iter(result.data.values()))
+        result_ids = {edge["node"]["id"] for edge in data["edges"]}
+        expected_ids = {entry.gid for entry in ACE.objects.filter(pk__in=[3])}
+        # assert expected results
+        self.assertSetEqual(result_ids, expected_ids)
